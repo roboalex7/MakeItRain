ddd5e81545c5b10670e708ec724a8424a19c5b7f
roboalex7 <alex@gtvinc.com>
2018-11-09 23:38:38 -0500

    Tweaked file to suit testing with Python libraries

--- MicrophoneTest/MicrophoneTest.ino
+++ MicrophoneTest/MicrophoneTest.ino
@@ -1,14 +1,23 @@
+/*  This program streams data from an analog microphone (Adafruit MAX9814)
+ *  to the serial plotter and converts the wave function to 
+ *  a positive amplitude. Implemented on an Arduino Leonardo (ATmega 32u4)
+ *  but should be compatible with any AVR microcontroller
+ *  https://www.adafruit.com/product/1713
+ *  
+ *  Make it Rain // AJW // 2018-11-03
+ */
+
 int sensorValue = 0;
-int groundState = 256;
-int maxVal = 512;
-int minVal = 0;
-int sampleWindow = 100;
+const int groundState = 256;
+const int maxVal = 512;
+const int minVal = 0;
+int sampleWindow = 0;
 
 
 
 void setup() 
 {
-  Serial.begin(9600);
+  Serial.begin(250000);
 }
 
 
@@ -40,11 +49,11 @@ void printData(int value1, int value2)
   //Print the desired value(s)
   Serial.print(value1); 
   Serial.print(",");
-  Serial.print(value2);
-  Serial.print(",");
+  //Serial.print(value2);
+  //Serial.print(",");
   //Print the ground state
-  Serial.print(groundState); 
-  Serial.print(",");
+  //Serial.print(groundState); 
+  //Serial.print(",");
   //Force upper and lower bounds to prevent autoscaling
   Serial.print(maxVal);
   Serial.print(",");
