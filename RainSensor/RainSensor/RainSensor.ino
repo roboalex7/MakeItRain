e25a4803a10f74ac90f508efa8b81490470a4990
roboalex7 <alex@gtvinc.com>
2018-12-04 23:09:22 -0500

    Added duplicate reading filter and conversion to drop rate

--- RainSensor/RainSensor/RainSensor.ino
+++ RainSensor/RainSensor/RainSensor.ino
@@ -1,6 +1,6 @@
 int sensorValue = 0;
 const int thresh = 450;
-const int sampleWindow = 30;
+const int sampleWindow = 10;
 const int wait = 5;
 const int samples = 1000;
 
@@ -24,12 +24,39 @@ void printData(int input[])
 int countDrops(int input[])
 {
   int drops = 0;
-  for (int i = 0; i < samples; i++)
+  for (int i = 3; i < samples; i++)
   {
-    if (input[i] > thresh || input[i] < 512 - thresh)
-      drops++; 
+    bool duplicate = false;
+    if (input[i] > thresh || input[i] < (512 - thresh))
+    {
+      for (int j = 1; j <= 3; j++)
+      {
+        if (input[i - j] > thresh || input[i-j] < (512 - thresh))
+        {
+          //Serial.println("Duplicate Point Detected");
+          duplicate = true;
+        }
+      }
+      if (!duplicate)
+        drops++;
+    }
   }
   return drops;
+}
+
+
+float dropsToIntensity(int drops)
+{
+  float volume = drops / 28.5;
+  Serial.print("Volume = ");
+  Serial.println(volume);
+  float depth = volume / 35.91; //Area of sensor in cm^2
+  Serial.print("Depth = ");
+  Serial.println(depth);
+  float rate = (depth * 360);
+  Serial.print("Rate = ");
+  Serial.print(rate);
+  Serial.println("cm/hr");
 }
 
 
@@ -45,11 +72,12 @@ void loop()
       delay(wait);
     }
     //printData(data);
-    Serial.print(j);
-    Serial.print("-Drops: ");
-    Serial.println(countDrops(data));
+    //Serial.print(j);
+    //Serial.print("-Drops: ");
+    //Serial.println(countDrops(data));
     total += countDrops(data);
   }
   Serial.print("Total Drops:");
   Serial.println(total);
+  dropsToIntensity(total);
 }
